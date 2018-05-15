19afe7aab46f2618903cb3d516496bd73c31da22
unknown <masterit3000@gmail.com>
2018-05-15 11:10:17 +0700

    init

---
+++ src/main/java/com/qltd/backend/qltdbackend/QltdbackendApplication.java
@@ -0,0 +1,12 @@
+package com.qltd.backend.qltdbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QltdbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QltdbackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/qltd/backend/qltdbackend/QltdbackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qltd.backend.qltdbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QltdbackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
